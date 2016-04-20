d3b5da180acff86f734acd19d122406ad2914985
phamvantuan <phamvantuan@tma.com.vn>
2016-04-20 19:31:49 +0700

    2nd

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-$("#main").append("Peter Parker");
+//$("#main").append("Peter Parker");
+var awesomeThoughts = "I am Peter Parker and I am awesome!";
+console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("awesome","fun");
+$("#main").append(funThoughts);
+console.log(awesomeThoughts);
+console.log(funThoughts);

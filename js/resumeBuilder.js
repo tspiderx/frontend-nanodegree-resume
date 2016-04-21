9bf998eafb261cb4bb63ed884b5fe4fa01e5cdc9
tuanpham86 <tuan.phamvan86@gmail.com>
2016-04-21 23:41:02 +0700

    JSON

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,11 +11,13 @@ console.log(awesomeThoughts);
 console.log(funThoughts);
 */
 
+/*
 var formattedName = HTMLheaderName.replace("%data%", "Peter Parker");
 var formattedRole = HTMLheaderRole.replace("%data%", "Web slinger");
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
-
+*/
+/*
 var skills = ["run", "walk", "sling"];
 var bio = {
 	"name": "Peter Parker 2",
@@ -26,3 +28,89 @@ var bio = {
 };
 
 $("#main").append(bio.name);
+*/
+var work = {
+	"jobs": [
+		{
+			"employer": "photographer",
+			"title": "Title A",
+			"location": "Adelaide",
+			"dates": "20/01/1900 - 20/04/1992",
+			"description": "walk around"
+		},
+		{
+			"employer": "photographer",
+			"title": "Title B",
+			"location": "Melbourne",
+			"dates": "20/04/1902 - 20/04/1994",
+			"description": "sitting on chairs"
+		}
+	]
+}
+
+var projects = {
+	"projects": [
+		{
+			"title": "Project A",
+			"dates": 1991,
+			"description": "JavaSript typing",
+			"images": ["https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON","http://www.copterlabs.com/json-what-it-is-how-it-works-how-to-use-it/"]
+		},
+		{
+			"title": "Project B",
+			"dates": 1993,
+			"description": "JavaSript typing",
+			"images": ["https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiiyOO-laDMAhWG5KYKHfYUA7EQjRwIBw&url=https%3A%2F%2Fpixabay.com%2Fen%2Fbackground-image-colorful-art-967820%2F&psig=AFQjCNE-FqqeunyZmHUaNi64rF56QwCyJw&ust=1461343118053892"]
+		}
+	]
+}
+
+var bio = {
+	"name": "Project A",
+	"role": "web developer",
+	"welcomeMessage": "JavaSript typing",
+	"contacts": {
+		"mpobile": 19001956,
+		"email": "test@test.test",
+		"github": "githubUs",
+		"twitter": "@twitterN",
+		"location":	"Sydney"
+	},
+	"skills": ["code","test","read","write"],
+	"bioPic": "images/fry.jpg"
+}
+
+var education = {
+	"schools": [
+		{
+			"name": "Project B",
+			"city": 1993,
+			"degree": "Masters",
+			"majors": ["CS"],
+			"dates": 1990,
+			"url": "http:google.com"
+		},
+		{
+			"name": "Project B",
+			"city": 1993,
+			"degree": "BA",
+			"majors": ["CS"],
+			"dates": 1991,
+			"url": "http:google.com"
+		}
+	],
+	"onlineCourses": [
+		{
+		"title": "Basic JS",
+		"school": "Udacity",
+		"dates": 2016,
+		"url": "www.google.com"
+		},
+		{
+		"title": "Web development",
+		"school": "Udacity",
+		"dates": 2016,
+		"url": "www.google.com.vn"
+		}
+	]
+}

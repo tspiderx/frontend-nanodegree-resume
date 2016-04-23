d7354574d3f166773b929b6d70f62d02e9557119
tuanpham86 <tuan.phamvan86@gmail.com>
2016-04-24 00:51:07 +0700

    Flow 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,12 +12,6 @@ console.log(funThoughts);
 */
 
 /*
-var formattedName = HTMLheaderName.replace("%data%", "Peter Parker");
-var formattedRole = HTMLheaderRole.replace("%data%", "Web slinger");
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-*/
-/*
 var skills = ["run", "walk", "sling"];
 var bio = {
 	"name": "Peter Parker 2",
@@ -29,17 +23,33 @@ var bio = {
 
 $("#main").append(bio.name);
 */
+
+var bio = {
+	"name": "Peter Parker",
+	"role": "Web slinger",
+	"welcomeMessage": "Learning JavaSript",
+	"contacts": {
+		"mobile": 19001956,
+		"email": "test@test.test",
+		"github": "githubUs",
+		"twitter": "@twitterN",
+		"location":	"Sydney"
+	},
+	"skills": ["code","test","read","write"],
+	"bioPic": "images/fry.jpg"
+}
+
 var work = {
 	"jobs": [
 		{
-			"employer": "photographer",
+			"employer": "Student",
 			"title": "Title A",
 			"location": "Adelaide",
 			"dates": "20/01/1900 - 20/04/1992",
-			"description": "walk around"
+			"description": "walking around"
 		},
 		{
-			"employer": "photographer",
+			"employer": "Photographer",
 			"title": "Title B",
 			"location": "Melbourne",
 			"dates": "20/04/1902 - 20/04/1994",
@@ -63,21 +73,6 @@ var projects = {
 			"images": ["https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiiyOO-laDMAhWG5KYKHfYUA7EQjRwIBw&url=https%3A%2F%2Fpixabay.com%2Fen%2Fbackground-image-colorful-art-967820%2F&psig=AFQjCNE-FqqeunyZmHUaNi64rF56QwCyJw&ust=1461343118053892"]
 		}
 	]
-}
-
-var bio = {
-	"name": "Project A",
-	"role": "web developer",
-	"welcomeMessage": "JavaSript typing",
-	"contacts": {
-		"mpobile": 19001956,
-		"email": "test@test.test",
-		"github": "githubUs",
-		"twitter": "@twitterN",
-		"location":	"Sydney"
-	},
-	"skills": ["code","test","read","write"],
-	"bioPic": "images/fry.jpg"
 }
 
 var education = {
@@ -114,3 +109,55 @@ var education = {
 		}
 	]
 }
+
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+$("#topContacts").append(formattedMobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+$("#topContacts").append(formattedEmail);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+$("#topContacts").append(formattedTwitter);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+$("#topContacts").append(formattedGithub);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+$("#topContacts").append(formattedLocation);
+
+var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
+$("#header").append(formattedPic);
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage)
+$("#header").append(formattedWelcomeMsg);
+
+if (bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
+	
+	var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
+	$("#skills").append(formattedSkill);
+}
+
+function displayWork() {
+	for (job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+		var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
+		
+		var formattedWorkDates = HTMLworkDates.replace("%data%",work.jobs[job].dates);
+		var formattedWorkLocation = HTMLworkLocation.replace("%data%",work.jobs[job].location);
+		var formattedWorkDescription = HTMLworkDescription.replace("%data%",work.jobs[job].description);
+		$(".work-entry:last").append(formattedWorkDates);
+		$(".work-entry:last").append(formattedWorkLocation);
+		$(".work-entry:last").append(formattedWorkDescription);
+	}
+}
+displayWork();
